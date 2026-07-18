3f5230ca06d61bdcc705f515145dc4f10f721ba3
berguemzina-eng <berguemzina@gmail.com>
2026-07-18 17:06:46 +0200

    Add files via upload

---
+++ src/navigation/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import ContentScreen from '../screens/ContentScreen';
+import DrawerMenu from '../screens/DrawerMenu';
+import HizbAlFalahScreen from '../screens/HizbAlFalahScreen';
+import colors from '../theme/colors';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="HomeMain" component={HomeScreen} />
+      <Stack.Screen name="Content" component={ContentScreen} />
+      <Stack.Screen name="HizbAlFalah" component={HizbAlFalahScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        drawerContent={(props) => <DrawerMenu {...props} />}
+        screenOptions={{
+          headerShown: false,
+          drawerPosition: 'right', // el menú abre desde la derecha (natural para RTL)
+          drawerStyle: { width: '82%', backgroundColor: colors.cream },
+        }}
+      >
+        <Drawer.Screen name="Home" component={HomeStack} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
