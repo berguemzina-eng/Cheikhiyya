551cdde81a5f61bda8531a51bacea2bebb24a685
berguemzina-eng <berguemzina@gmail.com>
2026-07-23 11:17:46 +0200

    Add files via upload

---
+++ app/src/main/java/com/cheikhiyya/tariqa/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.cheikhiyya.tariqa.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF0B3D2E)
+val PrimaryLight = Color(0xFF155A43)
+val Gold = Color(0xFFC9A24B)
+val GoldLight = Color(0xFFE4C97A)
+val Cream = Color(0xFFFBF6EC)
+val TextDark = Color(0xFF1E2A24)
+val TextMuted = Color(0xFF5C6B63)
+val BorderColor = Color(0xFFE3D7BA)
+val LeafAccent = Color(0xFF3ECF8E) // verde esmeralda brillante، لعقد "شجرة السلسلة"
